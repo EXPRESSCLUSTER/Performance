05eebc60ed4bde1602a40ea5cdd40d23a1d12aaf
yoshiday01 <yuki.yoshida@nec.com>
2020-12-10 13:42:26 +0900

    2st commit

--- mirror/src/mdperf.h
+++ mirror/src/mdperf.h
@@ -2,5 +2,7 @@
 #define ROW_LEN 2048
 #define PATH_LEN 1024
 #define ERR_INVALID_PARAM 1
+#define NORMAL_END 0
+#define NUMBER_OF_ARGUMENTS 8
 
 int sendalert(char* label, int threshold, int times, int interval, char* method, char* path);
