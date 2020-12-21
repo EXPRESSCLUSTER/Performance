ce27f8488c3d51550d681452761e98d99e94831e
yoshiday01 <yuki.yoshida@nec.com>
2020-12-21 17:53:18 +0900

    6th_commit

--- mirror/src/perfchk.c
+++ mirror/src/perfchk.c
@@ -12,7 +12,7 @@ int main (
 {
     /* define variables */
     char label[LABEL_LEN];
-    char method[LABEL_LEN];   
+    char method[METHOD_LEN];   
     char path[PATH_LEN];
     int times = 0;
     int interval = 0;
@@ -21,26 +21,26 @@ int main (
     int i = 0;
     int ret = 0;
 
-    /*Initialize*/
-    memset(label,0,sizeof(label));
-    memset(method,0,sizeof(method));
-    memset(path,0,sizeof(path));
+    /* Initialize */
+    memset(label, '\0', sizeof(label));
+    memset(method, '\0', sizeof(method));
+    memset(path, '\0', sizeof(path));
 
-    /*Check arguments*/
+    /* Check arguments */
     if((argc == NUMBER_OF_ARGUMENTS)){
-        if(!strcmp(argv[1], "alert")){
-            strncpy(label, argv[2],LABEL_LEN);
+        if(!strcmp(argv[1], STR_ALERT)){
+            strncpy(label, argv[2], LABEL_LEN);
             threshold = atof(argv[3]);
             times = atoi(argv[4]);
             interval = atoi(argv[5]);
-            strncpy(method, argv[6],LABEL_LEN);
-            strncpy(path, argv[7],PATH_LEN);
+            strncpy(method, argv[6], METHOD_LEN);
+            strncpy(path, argv[7], PATH_LEN);
            
-            if(threshold <= 0 || times <= 0 || interval <= 0 ) {
-                printf("%d: Invalid parameter (threshold:%f, times:%d, interval:%d).\n", __LINE__,threshold,times,interval);                
+            if(threshold <= 0 || times <= 0 || interval <= 0) {
+                printf("%d: Invalid parameter (threshold:%f, times:%d, interval:%d).\n", __LINE__, threshold, times, interval);                
                 return ERR_INVALID_PARAM;
             }
-            if(strcmp(method,"syslog") && strcmp(method,"alert") && strcmp(method,"mail") && strcmp(method,"trap")) {
+            if(strcmp(method, STR_METHOD_SYSLOG) && strcmp(method, STR_METHOD_ALERT) && strcmp(method, STR_METHOD_MAIL) && strcmp(method, STR_METHOD_TRAP)) {
                 printf("%d: Invalid parameter (method:%s).\n", __LINE__, method);
                 return ERR_INVALID_PARAM;
             }
@@ -63,33 +63,36 @@ int main (
 }
 
 
-int
-sendalert
-(
+int sendalert (
     char *label,
-    float threshold, int times, int interval, char *method, char *path)
+    float threshold,
+    int times, 
+    int interval, 
+    char *method,
+    char *path
+    )
 {
     char *token = NULL;
     char tmp[ROW_LEN];
     int column = 0;
     FILE *fp = NULL;
     int i = 0;
-    int number_line=0;
-    float value=0;
+    int line_number = 0;
+    float value = 0;
     int counts = 0;
     char *command = "clplogcmd -m ";
     char str[STR_LEN];
     char pre_time[TIME_LEN];
     char cmd_line[CMD_LEN];
-    int ret=0;
+    int ret = 0;
     
-    /*Initialize*/
-    memset(tmp, 0, sizeof(tmp));
-    memset(str, 0, sizeof(str));
-    memset(pre_time, 0, sizeof(pre_time));
-    memset(cmd_line, 0, sizeof(cmd_line));
+    /* Initialize */
+    memset(tmp, '\0', sizeof(tmp));
+    memset(str, '\0', sizeof(str));
+    memset(pre_time, '\0', sizeof(pre_time));
+    memset(cmd_line, '\0', sizeof(cmd_line));
 
-    /*Output arguments*/
+    /* Output arguments */
     printf("label    : %s \n", label);
     printf("threshold: %f \n", threshold);
     printf("times    : %d \n", times);
@@ -101,15 +104,15 @@ sendalert
     fp = fopen(path, "r");
     if(fp == NULL) {
         printf("%d: File not found (%s).\n", __LINE__, path);
-        return ERR_INVALID_PARAM;
+        return ERR_OPEN_FILE;
     }
     else {
         fgets(tmp, sizeof(tmp), fp);
+        fclose(fp);
         token = strtok(tmp, "\",");
         if(token == NULL) {
             printf("%d: Wrong format.\n", __LINE__);
-            fclose(fp);
-            return ERR_NOT_FOUND;
+            return ERR_INVALID_FORMAT;
         }
         while(token != NULL) {
             if(!strcmp(token, label)) {
@@ -122,31 +125,31 @@ sendalert
             }
         }
     }
-    fclose(fp);  
    
-    /*infinite loop*/
     while (1) {
         fp = fopen(path, "r");
         if(fp == NULL) {
             printf("%d: File not found (%s).\n", __LINE__, path);
-            return ERR_INVALID_PARAM;
+            return ERR_OPEN_FILE;
         }
         else{
-            number_line = 0;
+            line_number = 0;
             while(fgets(tmp, sizeof(tmp), fp) != NULL) {
-                number_line++;
+                line_number++;
             }
-            if(number_line <= 1) {
-                printf("%d: Wrong format (%d).\n", __LINE__, number_line);
+            if(line_number <= 1) {
+                printf("%d: Wrong format (%d).\n", __LINE__, line_number);
                 fclose(fp);
                 goto loop;
             }
             fseek(fp, 0L, SEEK_SET);
-            for(i = 0; i < number_line - 1; i++) {
+            for(i = 0; i < line_number - 1; i++) {
                 fgets(tmp, sizeof(tmp), fp);
             }
             printf("%s\n",tmp);
         }
+        fclose(fp);
+        
         /* compare cur_time to pre_time */
         token = strtok(tmp, "\",");
         if(strlen(pre_time) == 0) {
@@ -154,8 +157,7 @@ sendalert
         }
         else{
             if(!strcmp(token, pre_time)) {
-                printf("Find the same line.\n", __LINE__);
-                fclose(fp);
+                printf("%d: Find the same line.\n", __LINE__);
                 goto loop;
             }
             else{
@@ -173,7 +175,6 @@ sendalert
                 token = strtok(NULL, "\",");
             }
         }
-        fclose(fp); 
  
         /* find times */
         if(threshold <= value) {
@@ -185,24 +186,22 @@ sendalert
 
         /* Over threshold */
         if(times == counts) {
-            sprintf(str,"Over threshold (label:%s, threshold:%f, times:%d).\n", label, threshold,  times);
-            sprintf(cmd_line,"%s \"%s\"  --%s\n", command, str, method);
+            sprintf(str,"Over threshold (label:%s, threshold:%f, times:%d).", label, threshold, times);
+            sprintf(cmd_line,"%s \"%s\" --%s > /dev/null 2>&1", command, str, method);
             ret = system(cmd_line);
 
             /* Error handling of system() */
             if(WIFEXITED(ret)) {
                 if(WEXITSTATUS(ret) == 0) {
-                    printf("%d: command success. \n", __LINE__);
+                    printf("%d: Command success. \n", __LINE__);
                 }
                 else{
-                    printf("%d: command failed (%d).\n",__LINE__, ret);
-                    fclose(fp);
+                    printf("%d: Command failed (%d).\n",__LINE__, ret);
                     return ERR_COMMAND_FAILED;
                 }
             }
             else{
                 printf("%d: system() failed (%d).\n",__LINE__, WIFEXITED(ret));
-                fclose(fp);
                 return ERR_INTERNAL;
             }
         }
--- mirror/src/perfchk.h
+++ mirror/src/perfchk.h
@@ -3,14 +3,24 @@
 #define LABEL_LEN 128
 #define ROW_LEN 1024
 #define PATH_LEN 1024
-#define STR_LEN 1024
-#define CMD_LEN 2048
+#define STR_LEN 128
+#define CMD_LEN 1024
 #define TIME_LEN 32
+#define METHOD_LEN 32
+
+#define STR_ALERT "alert"
+
+#define STR_METHOD_ALERT "alert"
+#define STR_METHOD_SYSLOG "syslog"
+#define STR_METHOD_MAIL "mail"
+#define STR_METHOD_TRAP "trap"
+
 
 #define SUCCESS 0
 #define ERR_INTERNAL 1
 #define ERR_INVALID_PARAM 2
-#define ERR_NOT_FOUND 3
-#define ERR_COMMAND_FAILED 4
+#define ERR_INVALID_FORMAT 3
+#define ERR_OPEN_FILE 4
+#define ERR_COMMAND_FAILED 5
 
 int sendalert(char *, float, int, int, char *, char *);
